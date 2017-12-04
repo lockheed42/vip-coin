0e96ad38d74bcd04e15acbab41063ffccdf10873
monkey01127 <slj242526>
2017-12-05 00:11:44 +0800

    [M] 新增页面之间的链接，和公共跳转的整合

--- html_tpl/vip-cion/js/common.js
+++ html_tpl/vip-cion/js/common.js
@@ -1,1 +1,12 @@
 const common_url = 'http://127.0.0.1:8020/vip-cion/static' 
+
+
+function jumpTo (name) {
+	let jumpUrl = 'http://' + window.location.host + '/vip-cion/pages/'
+	if (name) {
+		jumpUrl = jumpUrl + name
+		window.location.href = jumpUrl
+	} else {
+		window.history.back();
+	}
+}
